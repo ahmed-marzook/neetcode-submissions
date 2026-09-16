5972ae57e54e57b40174f13a20b676052347d82e
Ahmed Marzook <43218910+ahmed-marzook@users.noreply.github.com>
2026-09-16 08:56:27 +0100

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 10.2
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "hey"
+print(type(variable))
+variable = [212,12]
+print(type(variable))
